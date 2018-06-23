6242ead5eb354491e08af2d251c7ac85db02391a
Taras Vaskiv <codertarasvaskiv@quintagroup.org>
2018-06-23 18:29:02 +0300

    Apply abstract factory to db

--- openprocurement/bridge/contracting/databridge.py
+++ openprocurement/bridge/contracting/databridge.py
@@ -55,31 +55,14 @@ class Db(object):
     """ Database proxy """
 
     def __init__(self, config):
-        self.config = config
 
+        self.config = config
         self._backend = None
         self._db_name = None
         self._port = None
         self._host = None
 
-        if 'cache_host' in self.config:
-            import redis
-            self._backend = "redis"
-            self._host = self.config.get('cache_host')
-            self._port = self.config.get('cache_port') or 6379
-            self._db_name = self.config.get('cache_db_name') or 0
-            self.db = redis.StrictRedis(host=self._host, port=self._port,
-                                        db=self._db_name)
-            self.set_value = self.db.set
-            self.has_value = self.db.exists
-        else:
-            from lazydb import Db
-            self._backend = "lazydb"
-            self._db_name = self.config.get('cache_db_name') or 'databridge_cache_db'
-            self.db = Db(self._db_name)
-            self.set_value = self.db.put
-            self.has_value = self.db.has
-
+        self.db = eval(self.config['db'])()
 
     def get(self, key):
         return self.db.get(key)
@@ -89,6 +72,28 @@ class Db(object):
 
     def has(self, key):
         return self.has_value(key)
+
+
+class RedisDb(object):
+    def __init__(self):
+        import redis
+        self._backend = "redis"
+        self._host = self.config.get('cache_host')
+        self._port = self.config.get('cache_port') or 6379
+        self._db_name = self.config.get('cache_db_name') or 0
+        self.db = redis.StrictRedis(host=self._host, port=self._port,
+                                    db=self._db_name)
+        self.set_value = self.db.set
+        self.has_value = self.db.exists
+
+class LazyDb(object):
+    def __init__(self):
+        from lazydb import Db
+        self._backend = "lazydb"
+        self._db_name = self.config.get('cache_db_name') or 'databridge_cache_db'
+        self.db = Db(self._db_name)
+        self.set_value = self.db.put
+        self.has_value = self.db.has
 
 
 def generate_req_id():
